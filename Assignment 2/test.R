7d4f0a94615f03dc76d645a9c4e5108380d8e90c
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2024-02-17 23:35:27 +0530

    Ass 2 complete

--- Assignment 2/test.R
+++ Assignment 2/test.R
@@ -1,10 +1,10 @@
 library(r2r)
-check = function(n, func) {
-  
+
+testUnif = function(n, func){
   freq = hashmap(
     on_missing_key = "default",
     default = 0
-    )
+  )
   
   redo = 10000
   for (i in 1:(redo * factorial(n))){
@@ -16,8 +16,13 @@ check = function(n, func) {
   
   if (length(freq_vals) != factorial(n)) return(FALSE)
   
-  if (chisq.test(freq_vals)$p.value > 0.05) return(TRUE)
-  else return(FALSE)
+  return(chisq.test(freq_vals)$p.value)
 }
 
-check(3, sample)
+check = function(arrSize, test, funct) {
+  p_val = test(arrSize, funct)
+  
+  return(ifelse(p_val > 0.05, TRUE, FALSE))
+}
+
+check(3, testUnif, sample)
